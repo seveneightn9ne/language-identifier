06b6002650d499e859a687cca1a5805e06bbfb36
Miles Steele <miles@milessteele.com>
2013-06-29 17:33:58 -0400

    cleanup

--- language-identifier.py
+++ language-identifier.py
@@ -1,19 +1,22 @@
+from language import Language
+
 
 class Question(object):
-    def __init__(question="", choices=None):
+    def __init__(self, question="", choices=None):
         self.question = question
         self.choices = choices or []
 
-    def ask():
-        print(question + "\n")
+    def ask(self):
+        print(self.question + "\n")
         for i, choice in enumerate(choices): print(choice)
+
         def _ask():
             try:
-                answer = int(raw_input(question))
+                answer = int(raw_input(self.question))
             except ValueError:
                 print("Not a valid number")
                 return _ask()
-            if choices.size() < answer+1:
+            if len(self.choices) < answer + 1:
                 print("Not a valid number")
                 return _ask()
             return answer
@@ -23,26 +26,33 @@ class Question(object):
     # def analyze_answer():
 
 
-class Language(object):
-    def __init__(name="", answers=None):
-        self.name = name
-        self.answers = answers
+# class Language(object):
+#     def __init__(name="", answers=None):
+#         self.name = name
+#         self.answers = answers
 
-    def response_to(self, question):
-        if answers.contains(question): return self.answers[question]
-        return None
+#     def response_to(self, question):
+#         if answers.contains(question): return self.answers[question]
+#         return None
 
-    def add_answer(self, question, answer):
-        pass
+#     def add_answer(self, question, answer):
+#         pass
 
-questions = [Question("How do you define a function?",
-                        ["def foo(bar, baz): ...", 
-                         "function foo(bar, baz) { ... }", 
-                         "There are no functions in this language",
-                         "I don't know / other"]
-                     )]
-possible_languages = [Language("Python", {questions[0]: 0}), Language("JavaScript")]
+# questions = [Question("How do you define a function?",
+#                         ["def foo(bar, baz): ...", 
+#                          "function foo(bar, baz) { ... }", 
+#                          "There are no functions in this language",
+#                          "I don't know / other"]
+#                      )]
+# possible_languages = [Language("Python", {questions[0]: 0}), Language("JavaScript")]
 
+
+def findin(f, l):
+    """ Like filter() but short-circuiting and returns a boolean """
+    for e in l:
+        if f(e):
+            return True
+    return False
 
 
 def filter_with_question(question, possible_languages):
@@ -68,4 +78,9 @@ def run_game(all_languages, all_questions):
 
 
 if __name__ == "__main__":
+    parens = Question("Are there parens?", {
+        'yes': "Yes.",
+        'no' : "No.",
+        })
+    languages = !TBD!
     run_game()
