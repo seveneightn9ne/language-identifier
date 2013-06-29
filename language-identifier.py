a255efc09f7c9c56fb45b2f6d7044428b5bf498e
Miles Steele <miles@milessteele.com>
2013-06-29 17:41:21 -0400

    use external Question

--- language-identifier.py
+++ language-identifier.py
@@ -1,51 +1,5 @@
 from language import Language
-
-
-class Question(object):
-    def __init__(self, question="", choices=None):
-        self.question = question
-        self.choices = choices or []
-
-    def ask(self):
-        print(self.question + "\n")
-        for i, choice in enumerate(self.choices): print(choice)
-
-        def _ask():
-            try:
-                answer = int(raw_input(self.question))
-            except ValueError:
-                print("Not a valid number")
-                return _ask()
-            if len(self.choices) < answer + 1:
-                print("Not a valid number")
-                return _ask()
-            return answer
-
-        return _ask()
-
-    # def analyze_answer():
-
-
-# class Language(object):
-#     def __init__(name="", answers=None):
-#         self.name = name
-#         self.answers = answers
-
-#     def response_to(self, question):
-#         if answers.contains(question): return self.answers[question]
-#         return None
-
-#     def add_answer(self, question, answer):
-#         pass
-
-# questions = [Question("How do you define a function?",
-#                         ["def foo(bar, baz): ...", 
-#                          "function foo(bar, baz) { ... }", 
-#                          "There are no functions in this language",
-#                          "I don't know / other"]
-#                      )]
-# possible_languages = [Language("Python", {questions[0]: 0}), Language("JavaScript")]
-
+from question import Question
 
 def findin(f, l):
     """ Like filter() but short-circuiting and returns a boolean """
